73ac839fbc6b772f8dc4471c7c93da3fdd039ebf
zoro1985 <251556768@qq.com>
2017-09-26 16:00:40 +0800

    //新增文件

--- db-delegate/src/test/java/org/inchain/DBModuleTest.java
+++ db-delegate/src/test/java/org/inchain/DBModuleTest.java
@@ -3,32 +3,44 @@ package org.inchain;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+import org.inchain.util.log.Log;
+import org.junit.Before;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.io.FileNotFoundException;
 
 /**
  * Unit test for simple DBModule.
  */
-public class DBModuleTest
-        extends TestCase {
+public class DBModuleTest extends TestCase {
     /**
      * Create the test case
      *
      * @param testName name of the test case
      */
-    public DBModuleTest(String testName) {
-        super(testName);
-    }
+
+    public static ClassPathXmlApplicationContext applicationContext;
 
     /**
-     * @return the suite of tests being tested
+     * start spring
      */
-    public static Test suite() {
-        return new TestSuite(DBModuleTest.class);
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp() {
-        assertTrue(true);
+    @Before
+    public static void init() {
+        //加载spring环境
+        if (null != applicationContext) {
+            return;
+        }
+        Log.info("get application context");
+        try {
+            applicationContext = new ClassPathXmlApplicationContext("classpath:/applicationContext.xml");
+        } catch (Exception e) {
+            if (e instanceof FileNotFoundException) {
+                Log.error("There is not applicationContext.xml in classpath!", e);
+            } else {
+                Log.error("", e);
+            }
+            return;
+        }
+        Log.info("System is started!");
     }
 }
